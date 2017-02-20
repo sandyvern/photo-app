7a656fb6cd054251144539cd991a0feb5a558ad1
Sandy <SVern99@aol.com>
2017-02-20 21:15:09 +0000

    Getting Stripe functionality to work

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -11,11 +11,12 @@ class Payment < ActiveRecord::Base
   end
   
   def process_payment
-    customer = Stripe::Customer.create email: email, source: token
+    customer = Stripe::Customer.create email: email, card: token
     
     Stripe::Charge.create customer: customer.id, 
                           amount: 1000, 
                           description: 'Premium', 
                           currency: 'usd'
   end
+  
 end
